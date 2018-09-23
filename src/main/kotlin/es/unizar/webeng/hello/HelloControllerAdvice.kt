16135e01d0d62764f334a959f91f6a3f40548c5a
lAngel <info@langelp.net>
2018-09-23 22:23:41 +0200

    Catch exception and return Error 400 (Bad Request)

--- src/main/kotlin/es/unizar/webeng/hello/HelloControllerAdvice.kt
+++ src/main/kotlin/es/unizar/webeng/hello/HelloControllerAdvice.kt
@@ -1,10 +1,11 @@
-package es.unizar.webeng.hello;
+package es.unizar.webeng.hello
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.ResponseBody
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.servlet.ModelAndView
 
 @ControllerAdvice
 class HelloControllerAdvice {
@@ -17,7 +18,10 @@ class HelloControllerAdvice {
 	@ResponseBody
 	@ExceptionHandler(InvalidWelcomeMessageException::class)
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
-	fun employeeNotFoundHandler(ex: InvalidWelcomeMessageException) : Message {
-		return Message(ex.message);
+	fun invalidWelcomeMessageHandler(ex: InvalidWelcomeMessageException) : ModelAndView{
+		val modelAndView = ModelAndView("welcome")
+		modelAndView.model["message"] = ex.getMsg()
+		modelAndView.model["time"] = ex.getMsg().getTime()
+		return modelAndView
 	}
 }
